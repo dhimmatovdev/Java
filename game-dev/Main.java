7511cbb000ded9b3ceffd85043c6837b8222431d
Dilshod Himmatov <dilshodhimmatov06@gmail.com>
2026-01-07 00:06:16 +0500

    game-dev uchun

---
+++ game-dev/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main{
+    public static void main(String[]args){
+        JOptionPane.showMessageDialog(
+            null,
+            "Bu yerda xabar yoziladi",
+            "Xabar sarlavhasi",
+            JOptionPane.INFORMATION_MESSAGE
+        );
+
+    }
+}
